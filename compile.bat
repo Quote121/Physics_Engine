13ee33fb3e80c8ebc71bc1b5ca7d8de07c5afb4c
Matt Bannister <matt.bann@live.co.uk>
2024-10-03 16:18:23 +0100

    Ran and fixed bugs in compile.py

--- compile.bat
+++
@@ -1,3 +0,0 @@
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
-mingw32-make.exe
-.\src\physicsMain.exe
--- compile.py
+++ compile.py
@@ -1,40 +1,57 @@
 ## Ability to cross compile code
 import os
 import subprocess
+import shutil
 import argparse
 
 EXECNAME = "physicsMain.exe"
 BUILDDIR = "build"
 WINDOWS: bool
 LINUX: bool
-global args
+
+## My overrided print
+def write(msg: str) -> None:
+    ## Get just the filename from the whole file path
+    pos = __file__.rfind('\\')
+    filename = __file__[pos+1:len(__file__)]
+    print(f"{filename} : [" + msg + ']')
 
 
 ## Will print error message and terminate process
 def error(errorMsg: str) -> None:
-    print("Error: '", errorMsg, "'")
+    write("Error: '", errorMsg, "'")
     quit()
 
 
 ## Helper method, will print if verbose mode active
 def verbose(msg: str) -> None:
     if (args.verbose):
-        print(msg)
+        write(msg)
 
 
 ## Will remove directory and all contents provided by dir
 def clean(dir: str) -> None:
+
     verbose(f"Cleaning directory: {dir}")
 
-    if WINDOWS:
-        ## CMD built in, powershell will fail
-        subprocess.run(["rmdir", "/s", "/q", f"./{dir}/"])
-    elif LINUX:
-        subprocess.run(["rm", "-rf", f"./{dir}/"])
+    if not (os.path.exists(dir)):
+        write("Clean() dir not found")
+        return
+
+    shutil.rmtree(dir)
 
 
 def main(parser: argparse.ArgumentParser) -> None:
+    global args
     args = parser.parse_args()
+
+    if (args.run and args.gdb):
+        write("Cannot use 'run' and 'run gdb' at the same time.")
+        quit()
+
+    ## Clean
+    if (args.clean):
+        clean(BUILDDIR)
 
     ## Determine if there is a build folder, if not create it
     if not (os.path.exists(f"./{BUILDDIR}/")):
@@ -43,17 +60,13 @@ def main(parser: argparse.ArgumentParser) -> None:
         
     else:
         verbose("Build dir found.")
-        ## Clean
-        if (args.clean):
-            clean(BUILDDIR)
-
-
+        
     ## cd build
     os.chdir(f"./{BUILDDIR}/")
     verbose("Entered build dir.")
 
     if WINDOWS:
-        cmakeArgs: str
+        cmakeArgs = ""
 
         if (args.debug):
             cmakeArgs += "-DCMAKE_BUILD_TYPE=debug"
@@ -66,20 +79,21 @@ def main(parser: argparse.ArgumentParser) -> None:
         verbose("Running make.")
         subprocess.run(["mingw32-make.exe"])
 
-    if LINUX:
+    elif LINUX:
         error("Not implemented.")
 
-
     ## Run program if specified
-
     if (args.run):
         verbose(f"Running application {EXECNAME}")
         subprocess.run([f"./src/{EXECNAME}"])
 
+    if (args.gdb):
+        verbose(f"Running gdb on {EXECNAME}")
+        subprocess.run(["gdb", f"./src/{EXECNAME}"])
+
     ## Leave the build dir
     verbose(f"Exiting {BUILDDIR}")
     os.chdir("../")
-    print("Finished.")
     
 
 
@@ -102,9 +116,10 @@ if __name__ == "__main__":
         prog="Project compile script",
         description="Supply various compilation options"
     )
-    parser.add_argument('run', '-r', '--run', help="Will run the executable after compilation", action='store_true') 
-    parser.add_argument('clean', '-c', '--clean', help="Will clean the project and compile from scratch", action='store_true')
-    parser.add_argument('verbose', '-v', '--verbose', help="Print out detailed information", action='store_true')
-    parser.add_argument('debug', '-g', '--debug', help="Enable debugging symbols", action='store_true')
+    parser.add_argument('-r', '--run', help="Will run the executable after compilation", action='store_true') 
+    parser.add_argument('-c', '--clean', help="Will clean the project and compile from scratch", action='store_true')
+    parser.add_argument('-v', '--verbose', help="Print out detailed information", action='store_true')
+    parser.add_argument('-d', '--debug', help="Enable debugging symbols", action='store_true')
+    parser.add_argument('-g', '--gdb', help="Enable gdb", action='store_true')
 
     main(parser)
